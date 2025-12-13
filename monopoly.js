f327379afad383970fa4e9de8e5b8d17d5ff1564
magagdan <magagdan@gmail.com>
2025-12-13 19:19:35 +0700

    Update monopoly.js

--- monopoly.js
+++ monopoly.js
@@ -2864,7 +2864,7 @@ window.onload = function() {
 	document.getElementById("enlarge20price").innerHTML += "<img src='images/free_parking_icon.png' height='80' width='72' alt='' style='position: relative; top: -20px;' />";
 	document.getElementById("enlarge38token").innerHTML += '<img src="images/tax_icon.png" height="60" width="70" alt="" style="position: relative; top: -20px;" />';
 
-	corrections();
+	// corrections();
 
 	// Jail corrections
 	$("<div>", {id: "jailpositionholder" }).appendTo("#jail");
